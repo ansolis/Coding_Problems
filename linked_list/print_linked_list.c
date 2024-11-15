995ebd27a44e899d1e941db287a04ce7532bbcea
Michael Ansolis <ansolis@gmail.com>
2024-11-14 23:27:08 -0800

    Update the file description

--- linked_list/print_linked_list.c
+++ linked_list/print_linked_list.c
@@ -1,9 +1,4 @@
-// GCC for VSCode setup guide: https://code.visualstudio.com/docs/cpp/config-mingw
-// To execute the program built from this file, do Ctrl-Shift-P, select
-// C/C++:Run C/C++ File. You should see the output in a new TERMINAL window.
-// If you click on the "play"/triangle button with "Run Code" selected in the
-// upper right-hand corner of the window, it will build and run the program,
-// but the output will go to the OUTPUT window instead of the TERMINAL window.
+// THis example creates a linked list and prints its nodes.
 
 #include <stdio.h>
 #include <stdlib.h>
