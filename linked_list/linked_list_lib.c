6beb42511abc3bff3e42a50ddae6625da45f47c4
Michael Ansolis <ansolis@gmail.com>
2025-06-14 19:34:27 -0700

    Add new function to the linked list library

--- linked_list/linked_list_lib.c
+++ linked_list/linked_list_lib.c
@@ -6,14 +6,14 @@ typedef struct Node {
     struct Node* next;
 } Node_t;
 
-static Node_t* create_node(int value) {
+Node_t* create_node(int value) {
     Node_t* node = (Node_t*)malloc(sizeof(Node_t));
     node->value  = value;
     node->next   = NULL;
     return node;
 }
 
-static Node_t* create_linked_list(int values[], int size) {
+Node_t* create_linked_list(int values[], int size) {
     Node_t* head = NULL;
     Node_t* node = NULL;
     Node_t* prev = NULL;
@@ -30,6 +30,23 @@ static Node_t* create_linked_list(int values[], int size) {
     return head;
 }
 
+Node_t* insert_linked_list_node(Node_t* old_head, Node_t* new_head) {
+    if (new_head != NULL) {
+        new_head->next = old_head;
+    }
+
+    return new_head;
+}
+
+Node_t* insert_linked_list_val(Node_t* old_head, int val) {
+    Node_t* new_head = create_node(val);
+    if (new_head != NULL) {
+        new_head->next = old_head;
+    }
+
+    return new_head;
+}
+
 void delete_linked_list(Node_t** head) {
     Node_t* node = *head;
     Node_t* next = NULL;
@@ -40,6 +57,51 @@ void delete_linked_list(Node_t** head) {
     }
 
     *head = NULL;
+}
+
+// Delete node at position pos. pos=0 is for the head (first node)
+// and pos=-1 is for the tail (last node).
+void delete_linked_list_node(Node_t** head, int pos) {
+    if (head == NULL) {
+        return;
+    }
+
+    Node_t* node      = *head;
+    Node_t* next      = (node == NULL) ? NULL : node->next;
+    Node_t* next_next = (next == NULL) ? NULL : next->next;
+
+    if (node == NULL) {
+        return;
+    }
+
+    // Delete the tail (last node)
+    if (pos == -1) {
+        if (next != NULL) {
+            while (next_next != NULL) {
+                next_next = next_next->next;
+                next      = next->next;
+                node      = node->next;
+            }
+
+            if (next != NULL) {
+                // Now next points to tail and node points to the node before tail
+                free(next);
+                node->next = NULL;
+            }
+        } else {
+            // All we have left is the head node, so to delete the tail node we
+            // have to delete the head node
+            pos = 0;
+        }
+    }
+
+    // Delete the head node
+    if (pos == 0) {
+        *head = node->next;
+        free(node);
+    }
+
+    // TODO: Delete a node that's neither head nor tail
 }
 
 int get_node_count(Node_t* head) {
